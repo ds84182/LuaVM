6c76c7f4103b6630ab723bfaab50445fc9013c38
Dwayne Slater <ds84182@gmail.com>
2016-03-30 11:49:05 -0400

    VM Accuracy Test: Close Upvalue Test

---
+++ vm_accuracy_test/test_closure_upvalue_close.lua
@@ -0,0 +1,20 @@
+return function()
+	local getA, getB, getC
+	-- Simple Close (Only functional in 5.1)
+	do
+		local a = 32
+		function getA() return a end
+	end
+	-- Loop Close
+	do
+		for i=1, 128 do
+			if i == 65 then
+				function getB() return i end
+			end
+		end
+	end
+	-- Implicit Function Close
+	getC = (function(c) return function() return c end end)(123)
+	
+	return getA(), getB(), getC()
+end,{},{32, 65, 123}
